d6f3f1525184e9d18bf015b430787190600851ef
Futureblur <itsfutureblur@gmail.com>
2022-07-06 06:36:58 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+
+	files
+	{
+		"imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3.cpp"
+	}
+
+	sysincludedirs
+	{
+		"%{GlfwInclude}"
+	}
